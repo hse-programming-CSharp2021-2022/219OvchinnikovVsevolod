741438a6165d26f89b595e3967bf825b4077326f
Ovchinnikov Vsevolod <sevavov.2004@gmail.com>
2021-09-13 15:09:51 +0300

    Initialized CW_1

---
+++ MODULE_1/CW_1/Task10/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
