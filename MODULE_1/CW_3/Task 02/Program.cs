e49758811ec45285ea970a4f67ad75a8d7511705
Ovchinnikov Vsevolod <sevavov.2004@gmail.com>
2021-09-14 18:57:08 +0300

    Added

---
+++ MODULE_1/CW_3/Task 02/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Task_02
+{
+    class Program
+    {
+        public static bool Function1(bool p, bool q)
+        {
+            return !(p && q) && (!(p | !q));
+        }
+
+        public static void Function2(out bool x, bool p, bool q)
+        {
+            x = !(p && q) && (!(p | !q));
+            return;
+        }
+        static void Main(string[] args)
+        {
+            bool[] flags = {false, true};
+            for (var p = 0; p < flags.Length; p += 1)
+            {
+                for (var q = 0; q < flags.Length; q += 1)
+                {
+                    var t = Function1(flags[p], flags[q]);
+                    var f = false;
+                    Function2(out f, flags[p], flags[q]);
+                    Console.WriteLine($"{flags[p]} {flags[q]} {t} {f}");
+                }
+            }
+        }
+    }
+}
