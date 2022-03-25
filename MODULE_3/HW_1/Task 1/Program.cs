46bbaa737786c272580350187ed00b9f80ca5e26
Всеволод Овчинников <sevavov.2004@gmail.com>
2022-03-25 13:12:39 +0300

    Added HW_1

---
+++ MODULE_3/HW_1/Task 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task_1
+{
+    delegate int Cast(double a);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cast cast = new Cast((a) => (int)Math.Round(a));
+            Cast cast1 = new Cast((a) => (int)a);
+            Console.WriteLine(cast(3.261));
+            Console.WriteLine(cast1(3.261));
+            Console.WriteLine(cast(1.5));
+            Console.WriteLine(cast1(1.5));
+            Console.WriteLine();
+            Random rnd = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                double a = rnd.NextDouble() + rnd.Next(1, 5);
+                Console.WriteLine($"{a}\n{cast(a)}\n{cast1(a)}\n");
+            }
+        }
+    }
+}
---
+++ MODULE_3/HW_1/Task 3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task_3
+{
+    delegate double DelegateConvertTemperature(double a);
+    class TemperatureConverterImp
+    {
+        public double CelsiusToFahrenheit(double degC) => degC * 9 / 5.0 + 32;
+        public double FahrenheitToCelsius(double degF) => (degF - 32) * 5 / 9.0;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TemperatureConverterImp temperatureConverterImp = new();
+            DelegateConvertTemperature delegateConvertTemperature = temperatureConverterImp.CelsiusToFahrenheit;
+            delegateConvertTemperature += temperatureConverterImp.FahrenheitToCelsius;
+            double temp = 25;
+            foreach (var @delegate in delegateConvertTemperature.GetInvocationList())
+            {
+                var method = (DelegateConvertTemperature) @delegate;
+                Console.WriteLine($"Temp = {temp = method(temp)} " + (method.Method.Name == "CelsiusToFahrenheit" ? "F" : "C"));
+            }
+        }
+    }
+}
