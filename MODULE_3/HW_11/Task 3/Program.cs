3db09d5d26c84e04486c88b0af713f073dd4bf41
Всеволод Овчинников <sevavov.2004@gmail.com>
2022-03-26 08:43:26 +0300

    Added HW_11

---
+++ MODULE_3/HW_11/Task 3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new();
+            using FileStream fileStream = new FileStream("Numbers.txt", FileMode.Create, FileAccess.ReadWrite);
+            if (fileStream.Length == 0)
+            {
+                BinaryWriter bw = new(fileStream);
+                for (int i = 0; i < 10; i++)
+                    bw.Write(rand.Next(1, 101));
+            }
+            else
+            {
+                BinaryReader br = new(fileStream);
+                BinaryWriter bw = new(fileStream);
+                int[] nums = new int[10];
+                for (int i = 0; i < 10; i++)
+                {
+                    nums[i] = br.ReadInt32(); 
+                    Console.Write(nums[i] + " ");
+                }
+                int a = int.Parse(Console.ReadLine() ?? string.Empty);
+                int id = 0, minDiff = Math.Abs(a - nums[0]);
+                for (int i = 1; i < 10; i++)
+                    if (Math.Abs(a - nums[0]) < minDiff)
+                        id = i;
+                nums[id] = a;
+                fileStream.Position = 0;
+                for (int i = 0; i < 10; i++)
+                    bw.Write(nums[i]);
+                fileStream.Position = 0;
+                for (int i = 0; i < 10; i++)
+                {
+                    nums[i] = br.ReadInt32();
+                    Console.Write(nums[i] + " ");
+                }
+            }
+        }
+    }
+}
